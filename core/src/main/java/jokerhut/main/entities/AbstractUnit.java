7ba59ee5f6dad730715363eb7f270a1b34738e27
Dave <davidglogowski2003@gmail.com>
2025-09-15 00:36:20 +0200

    Add unit type enum

--- core/src/main/java/jokerhut/main/entities/AbstractUnit.java
+++ core/src/main/java/jokerhut/main/entities/AbstractUnit.java
@@ -11,58 +11,65 @@ public abstract class AbstractUnit {
     private float softAttack;
     private float hardAttack;
     private String name;
+    private String unitType;
     private Axial position;
 
     private TextureRegion sprite;
 
-
     public float getHealth() {
         return health;
     }
+
     public void setHealth(float health) {
         this.health = health;
     }
+
     public float getDefense() {
         return defense;
     }
+
     public void setDefense(float defense) {
         this.defense = defense;
     }
+
     public float getSoftAttack() {
         return softAttack;
     }
+
     public void setSoftAttack(float softAttack) {
         this.softAttack = softAttack;
     }
+
     public float getHardAttack() {
         return hardAttack;
     }
+
     public void setHardAttack(float hardAttack) {
         this.hardAttack = hardAttack;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public Axial getPosition() {
         return position;
     }
+
     public void setPosition(Axial position) {
         this.position = position;
     }
+
     public TextureRegion getSprite() {
         return sprite;
     }
+
     public void setSprite(TextureRegion sprite) {
         this.sprite = sprite;
     }
-
-
-
-
-
-
 
 }
---
+++ core/src/main/java/jokerhut/main/enums/UnitType.java
@@ -0,0 +1,26 @@
+package jokerhut.main.enums;
+
+public enum UnitType {
+
+    INFANTRY("Infantry"),
+    LIGHT_ARMOR("Light Armor"),
+    HEAVY_ARMOR("Heavy Armor"),
+    MEDIUM_ARMOR("Medium Armor"),
+    MACHINE_GUN_INFANTRY("Machine Gun Infantry");
+
+    private String displayName;
+
+    UnitType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName; // optional, so printing the enum gives the nice name
+    }
+
+}
